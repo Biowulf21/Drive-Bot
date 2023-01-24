29c07dc716481fd75a9807973acf3dd694ff5a85
James Jilhaney <notail2111@gmail.com>
2023-01-24 16:28:21 +0800

    cleaned up code

--- init_bot.py
+++ init_bot.py
@@ -1,2 +1,37 @@
+import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+
+
 def initialize():
-    pass
+    creds = None
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('drive', 'v3', credentials=creds)
+    
+    if os.path.isfile('subslist.xlsx'):
+        print('subslist.xlsx file found. Continuing...')
+    else:
+        print("No subslist.xlsx file found. Please create one and try again.")
+
+    return service
--- test-googledrive.py
+++ test-googledrive.py
@@ -4,46 +4,18 @@ import os.path
 import pandas as pd
 
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 from init_bot import initialize
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive']
 
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    
-    if os.path.isfile('subslist.xlsx'):
-        print('subslist.xlsx file found. Continuing...')
-    else:
-        print("No subslist.xlsx file found. Please create one and try again.")
+   # initializes the bot
+
+    service = initialize() 
 
     try:
-        service = build('drive', 'v3', credentials=creds)
         subs_excel_file = pd.read_excel("subslist.xlsx")
 
         id_numbers = subs_excel_file['ID NUMBER'].values
